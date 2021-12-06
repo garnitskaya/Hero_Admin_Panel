2740032e1fa8dd1c460acee1a8106722d7a52d9b
Alena <9972211@gmail.com>
2021-12-06 19:46:47 +0200

    Store enhancers

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,29 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import heroes from './../reducers/heroes';
 import filters from './../reducers/filters';
 
-const store = createStore(combineReducers({ heroes, filters }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const enhancer = (createStore) => (...arg) => {
+    const store = createStore(...arg);
+
+    const oldDispatch = store.dispatch;//сохраняем оригинальный dispatch, который принимал в себя только объект
+    store.dispatch = (action) => { //взяли оригинальный dispatch и перезаписали его
+        if (typeof action === 'string') { //если action будет строкой
+            return oldDispatch({ //вызываем оригинальный dispatch и в него передаём объект 
+                type: action
+            })
+        }
+        return oldDispatch(action);//если пришла не строка возвращаем dispatch
+    }
+    return store;
+}
+
+const store = createStore(
+    combineReducers({ heroes, filters }),
+    compose(
+        enhancer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
 export default store;
